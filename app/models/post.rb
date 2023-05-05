dd193081a69721934d4bc1ab57f03db8b1ec5d23
MelroyNoronha <melmax1208@gmail.com>
2023-05-05 18:37:10 +0530

    Fix validation in Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   MAX_TITLE_LENGTH = 125
   validates :title, presence: true, length: { maximum: MAX_TITLE_LENGTH }
-  validates :upvotes, numericality: { greater_than: :downvotes }
+  validates :upvotes, numericality: { greater_than_or_equal_to: :downvotes }
   validates_inclusion_of :is_blog_worthy, in: [true, false]
 end
