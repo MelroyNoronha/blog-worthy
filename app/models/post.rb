5ca8d1d82acf0edf8b0d6e78cf2e1f480b065a0f
MelroyNoronha <melmax1208@gmail.com>
2023-05-05 12:14:44 +0530

    Adds validations to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  MAX_TITLE_LENGTH = 125
+  validates :title, presence: true, length: { maximum: MAX_TITLE_LENGTH }
+  validates :upvotes, numericality: { greater_than: :downvotes }
+  validates_inclusion_of :is_blog_worthy, in: [true, false]
 end
