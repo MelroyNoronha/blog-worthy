3fcee102c8c60625286f94448afe9f2e4ba7d6b3
MelroyNoronha <melmax1208@gmail.com>
2023-05-05 22:13:11 +0530

    Added git hooks, eslint, prettier and rubocop

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
 import { setAuthHeaders } from "apis/axios";
@@ -12,7 +13,7 @@ const App = () => {
     setAuthHeaders(setLoading);
   }, []);
 
-  if(loading) {
+  if (loading) {
     return <h1>Loading...</h1>;
   }
 
