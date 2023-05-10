3dc432f8b4e741c60a54275d6bf383ac37d7f2a9
MelroyNoronha <melmax1208@gmail.com>
2023-05-10 23:33:50 +0530

    Add validations for slug and create set_slug method

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,32 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: MAX_TITLE_LENGTH }
   validates :upvotes, numericality: { greater_than_or_equal_to: :downvotes }
   validates_inclusion_of :is_blog_worthy, in: [true, false]
+  validates :slug, uniqueness: true
+  validate :slug_not_changed
+
+  before_create :set_slug
+
+  private
+
+    def set_slug
+      title_slug = title.parameterize
+      latest_task_slug = Post.where(
+        "slug REGEXP ?",
+        "^#{title_slug}$|^#{title_slug}-[0-9]+$",
+      ).order("LENGTH(slug) DESC", slug: :desc).first&.slug
+      slug_count = 0
+      if latest_task_slug.present?
+        slug_count = latest_task_slug.split("-").last.to_i
+        only_one_slug_exists = slug_count == 0
+        slug_count = 1 if only_one_slug_exists
+      end
+      slug_candidate = slug_count.positive? ? "#{title_slug}-#{slug_count + 1}" : title_slug
+      self.slug = slug_candidate
+    end
+
+    def slug_not_changed
+      if slug_changed? && self.persisted?
+        errors.add(:slug, t("task.slug.immutable"))
+      end
+    end
 end
