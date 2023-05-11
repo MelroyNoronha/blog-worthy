1daa4d0001fa04c8da69c9878e6fa4e6b1f0cacb
MelroyNoronha <melmax1208@gmail.com>
2023-05-11 17:06:25 +0530

    Improve Form component styling

--- app/javascript/src/components/Posts/Form.jsx
+++ app/javascript/src/components/Posts/Form.jsx
@@ -12,19 +12,23 @@ const Form = ({
   handleSubmit,
 }) => (
   <form className="mx-auto max-w-lg">
-    <Input
-      required
-      label="Title"
-      maxLength={125}
-      placeholder="Post Title (Max 125 Characters Allowed)"
-      value={title}
-      onChange={e => setTitle(e.target.value)}
-    />
-    <Textarea
-      label="Description"
-      value={description}
-      onChange={e => setDescription(e.target.value)}
-    />
+    <div className="my-4">
+      <Input
+        required
+        label="Title"
+        maxLength={125}
+        placeholder="Post Title (Max 125 Characters Allowed)"
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+      />
+    </div>
+    <div className="my-4">
+      <Textarea
+        label="Description"
+        value={description}
+        onChange={e => setDescription(e.target.value)}
+      />
+    </div>
     <Button
       label={type === "create" ? "Create Post" : "Update Post"}
       loading={loading}
