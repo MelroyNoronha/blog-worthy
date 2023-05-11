030ee5fe2e6381c930fba0ca6ab6a8d265c30eed
MelroyNoronha <melmax1208@gmail.com>
2023-05-11 22:29:11 +0530

    Improve styling
    Add cursor-pointer to post title.
    Add padding to post.

--- app/javascript/src/components/Table/TableRow.jsx
+++ app/javascript/src/components/Table/TableRow.jsx
@@ -5,10 +5,10 @@ import PropTypes from "prop-types";
 const TableRow = ({ data }) => (
   <tbody className="divide-y divide-gray-200 bg-white">
     {data.map(rowData => (
-      <tr key={rowData.id}>
+      <div className="py-4" key={rowData.id}>
         <td
-          className="block w-64 px-6 py-2 text-sm
-            font-medium capitalize leading-8 text-bb-purple"
+          className="block w-64 cursor-pointer px-6 py-2
+            text-sm font-medium capitalize leading-8 text-bb-purple"
         >
           {rowData.title}
         </td>
@@ -18,7 +18,7 @@ const TableRow = ({ data }) => (
         >
           {rowData.description}
         </td>
-      </tr>
+      </div>
     ))}
   </tbody>
 );
