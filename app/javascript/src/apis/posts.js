8f8c2613b2066834149075a65cb64d8d23ddf02a
MelroyNoronha <melmax1208@gmail.com>
2023-05-12 12:14:23 +0530

    Add show posts API

--- app/javascript/src/apis/posts.js
+++ app/javascript/src/apis/posts.js
@@ -2,11 +2,13 @@ import axios from "axios";
 
 const list = () => axios.get("/posts");
 
+const show = slug => axios.get(`/posts/${slug}`);
+
 const create = payload =>
   axios.post("/posts", {
     post: payload,
   });
 
-const postsApi = { list, create };
+const postsApi = { list, show, create };
 
 export default postsApi;
