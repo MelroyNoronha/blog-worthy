d305ed3de40ba29e045898ce44096519f58a8217
MelroyNoronha <melmax1208@gmail.com>
2023-05-24 13:56:43 +0530

    Add tests for Organization model

--- test/models/organization_test.rb
+++ test/models/organization_test.rb
@@ -3,7 +3,13 @@
 require "test_helper"
 
 class OrganizationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @organization = Organization.new(name: "Big Binary")
+  end
+
+  def test_organization_should_not_be_saved_without_organization
+    @organization.name = ""
+    assert_not @organization.valid?
+    assert_includes @organization.errors.full_messages, "Name can't be blank"
+  end
 end
